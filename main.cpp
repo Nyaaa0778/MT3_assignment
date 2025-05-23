a3c55553d0cbb5fcb8ecb57fb6de02c1c3b9adcf
Nyaaa0778 <k024g0093@g.neec.ac.jp>
2025-05-23 12:37:54 +0900

    引数を修正

--- main.cpp
+++ main.cpp
@@ -13,7 +13,8 @@ const float kWindowHeight = 720.0f;
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
   // ライブラリの初期化
-  Novice::Initialize(kWindowTitle, 1280, 720);
+  Novice::Initialize(kWindowTitle, static_cast<int>(kWindowWidth),
+                     static_cast<int>(kWindowHeight));
 
   // キー入力結果を受け取る箱
   char keys[256] = {0};
