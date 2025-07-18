c2ef453fae816519229e73070baaf03ac8dcb01b
Nyaaa0778 <k024g0093@g.neec.ac.jp>
2025-07-18 10:54:49 +0900

    取り組み前

---
+++ MathUtility.cpp
@@ -0,0 +1,179 @@
+﻿#include"MathUtility.h"
+
+/// <summary>
+/// 行列の和
+/// </summary>
+/// <param name="m1"></param>
+/// <param name="m2"></param>
+/// <returns></returns>
+Matrix4x4 Add(const Matrix4x4 &m1, const Matrix4x4 &m2) {
+  Matrix4x4 result;
+
+  for (int i = 0; i < 4; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      result.m[i][j] = m1.m[i][j] + m2.m[i][j];
+    }
+  }
+
+  return result;
+}
+
+/// <summary>
+/// 行列の差
+/// </summary>
+/// <param name="m1"></param>
+/// <param name="m2"></param>
+/// <returns></returns>
+Matrix4x4 Subtract(const Matrix4x4 &m1, const Matrix4x4 &m2) {
+  Matrix4x4 result;
+
+  for (int i = 0; i < 4; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      result.m[i][j] = m1.m[i][j] - m2.m[i][j];
+    }
+  }
+
+  return result;
+}
+
+/// <summary>
+/// 行列の積
+/// </summary>
+/// <param name="m1"></param>
+/// <param name="m2"></param>
+/// <returns></returns>
+Matrix4x4 Multiply(const Matrix4x4 &m1, const Matrix4x4 &m2) {
+  Matrix4x4 result;
+
+  for (int i = 0; i < 4; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      result.m[i][j] = 0;
+      for (int k = 0; k < 4; ++k) {
+        result.m[i][j] += m1.m[i][k] * m2.m[k][j];
+      }
+    }
+  }
+
+  return result;
+}
+
+/// <summary>
+/// 3次元ベクトルの和
+/// </summary>
+/// <param name="v1"></param>
+/// <param name="v2"></param>
+/// <returns></returns>
+Vector3 Add(const Vector3 &v1, const Vector3 v2) {
+  Vector3 result;
+  result.x = v1.x + v2.x;
+  result.y = v1.y + v2.y;
+  result.z = v1.z + v2.z;
+
+  return result;
+}
+
+/// <summary>
+/// 3次元ベクトルの差
+/// </summary>
+/// <param name="v1"></param>
+/// <param name="v2"></param>
+/// <returns></returns>
+Vector3 Subtract(const Vector3 v1, const Vector3 v2) {
+  Vector3 result;
+  result.x = v1.x - v2.x;
+  result.y = v1.y - v2.y;
+  result.z = v1.z - v2.z;
+
+  return result;
+}
+
+/// <summary>
+/// 3次元ベクトルの積
+/// </summary>
+/// <param name="s"></param>
+/// <param name="v"></param>
+/// <returns></returns>
+Vector3 Multiply(float s, const Vector3 &v) {
+  Vector3 result;
+  result.x = v.x * s;
+  result.y = v.y * s;
+  result.z = v.z * s;
+
+  return result;
+}
+
+/// ========================================
+/// 演算子のオーバーロード
+/// ========================================
+
+/// <summary>
+/// 3次元ベクトルの和
+/// </summary>
+/// <param name="v1"></param>
+/// <param name="v2"></param>
+/// <returns></returns>
+Vector3 operator+(const Vector3 &v1, const Vector3 &v2) { return Add(v1, v2); }
+
+/// <summary>
+/// 3次元ベクトルの差
+/// </summary>
+/// <param name="v1"></param>
+/// <param name="v2"></param>
+/// <returns></returns>
+Vector3 operator-(const Vector3 &v1, const Vector3 &v2) {
+  return Subtract(v1, v2);
+}
+
+/// <summary>
+/// 3次元ベクトルの積
+/// </summary>
+/// <param name="v"></param>
+/// <param name="s"></param>
+/// <returns></returns>
+Vector3 operator*(const Vector3 &v, float s) { return Multiply(s, v); }
+
+/// <summary>
+/// 3次元ベクトルの積
+/// </summary>
+/// <param name="v"></param>
+/// <param name="s"></param>
+/// <returns></returns>
+Vector3 operator*(float s, const Vector3 &v) { return v * s; }
+
+/// <summary>
+/// 3次元ベクトルの商
+/// </summary>
+/// <param name="v"></param>
+/// <param name="s"></param>
+/// <returns></returns>
+Vector3 operator/(const Vector3 &v, float s) { return Multiply(1.0f / s, v); }
+
+/// <summary>
+/// 行列の和
+/// </summary>
+/// <param name="m1"></param>
+/// <param name="m2"></param>
+/// <returns></returns>
+Matrix4x4 operator+(const Matrix4x4 &m1, const Matrix4x4 &m2) {
+  return Add(m1, m2);
+}
+
+/// <summary>
+/// 行列の差
+/// </summary>
+/// <param name="m1"></param>
+/// <param name="m2"></param>
+/// <returns></returns>
+Matrix4x4 operator-(const Matrix4x4 &m1, const Matrix4x4 &m2) {
+  return Subtract(m1, m2);
+}
+
+/// <summary>
+/// 行列の積
+/// </summary>
+/// <param name="m1"></param>
+/// <param name="m2"></param>
+/// <returns></returns>
+Matrix4x4 operator*(const Matrix4x4 m1, const Matrix4x4 &m2) {
+  return Multiply(m1, m2);
+}
---
+++ MathUtility.h
@@ -0,0 +1,182 @@
+﻿#pragma once
+
+struct Vector3 {
+  float x;
+  float y;
+  float z;
+
+  /// <summary>
+  /// 和の複合演算子
+  /// </summary>
+  /// <param name="v"></param>
+  /// <returns></returns>
+  Vector3 &operator+=(const Vector3 &v) {
+    x += v.x;
+    y += v.y;
+    z += v.z;
+
+    return *this;
+  }
+
+  /// <summary>
+  /// 差の複合演算子
+  /// </summary>
+  /// <param name="v"></param>
+  /// <returns></returns>
+  Vector3 &operator-=(const Vector3 &v) {
+
+    x -= v.x;
+    y -= v.y;
+    z -= v.z;
+
+    return *this;
+  }
+
+  /// <summary>
+  /// 積の複合演算子
+  /// </summary>
+  /// <param name="v"></param>
+  /// <returns></returns>
+  Vector3 &operator*=(const Vector3 &v) {
+    x *= v.x;
+    y *= v.y;
+    z *= v.z;
+
+    return *this;
+  }
+
+  /// <summary>
+  /// 商の複合演算子
+  /// </summary>
+  /// <param name="v"></param>
+  /// <returns></returns>
+  Vector3 &operator/=(const Vector3 &v) {
+    x /= v.x;
+    y /= v.y;
+    z /= v.z;
+
+    return *this;
+  }
+};
+
+struct Matrix4x4 {
+  float m[4][4];
+};
+
+/// <summary>
+/// 行列の和
+/// </summary>
+/// <param name="m1"></param>
+/// <param name="m2"></param>
+/// <returns></returns>
+Matrix4x4 Add(const Matrix4x4 &m1, const Matrix4x4 &m2);
+
+/// <summary>
+/// 行列の差
+/// </summary>
+/// <param name="m1"></param>
+/// <param name="m2"></param>
+/// <returns></returns>
+Matrix4x4 Subtract(const Matrix4x4 &m1, const Matrix4x4 &m2);
+
+/// <summary>
+/// 行列の積
+/// </summary>
+/// <param name="m1"></param>
+/// <param name="m2"></param>
+/// <returns></returns>
+Matrix4x4 Multiply(const Matrix4x4 &m1, const Matrix4x4 &m2);
+
+/// <summary>
+/// 3次元ベクトルの和
+/// </summary>
+/// <param name="v1"></param>
+/// <param name="v2"></param>
+/// <returns></returns>
+Vector3 Add(const Vector3 &v1, const Vector3 v2);
+
+/// <summary>
+/// 3次元ベクトルの差
+/// </summary>
+/// <param name="v1"></param>
+/// <param name="v2"></param>
+/// <returns></returns>
+Vector3 Subtract(const Vector3 v1, const Vector3 v2);
+
+/// <summary>
+/// 3次元ベクトルの積
+/// </summary>
+/// <param name="s"></param>
+/// <param name="v"></param>
+/// <returns></returns>
+Vector3 Multiply(float s, const Vector3 &v);
+
+
+
+/// ========================================
+/// 演算子のオーバーロード
+/// ========================================
+
+/// <summary>
+/// 3次元ベクトルの和
+/// </summary>
+/// <param name="v1"></param>
+/// <param name="v2"></param>
+/// <returns></returns>
+Vector3 operator+(const Vector3 &v1, const Vector3 &v2);
+
+/// <summary>
+/// 3次元ベクトルの差
+/// </summary>
+/// <param name="v1"></param>
+/// <param name="v2"></param>
+/// <returns></returns>
+Vector3 operator-(const Vector3 &v1, const Vector3 &v2);
+
+/// <summary>
+/// 3次元ベクトルの積
+/// </summary>
+/// <param name="v"></param>
+/// <param name="s"></param>
+/// <returns></returns>
+Vector3 operator*(const Vector3 &v, float s);
+
+/// <summary>
+/// 3次元ベクトルの積
+/// </summary>
+/// <param name="v"></param>
+/// <param name="s"></param>
+/// <returns></returns>
+Vector3 operator*(float s, const Vector3 &v);
+
+/// <summary>
+/// 3次元ベクトルの商
+/// </summary>
+/// <param name="v"></param>
+/// <param name="s"></param>
+/// <returns></returns>
+Vector3 operator/(const Vector3 &v, float s);
+
+/// <summary>
+/// 行列の和
+/// </summary>
+/// <param name="m1"></param>
+/// <param name="m2"></param>
+/// <returns></returns>
+Matrix4x4 operator+(const Matrix4x4 &m1, const Matrix4x4 &m2);
+
+/// <summary>
+/// 行列の差
+/// </summary>
+/// <param name="m1"></param>
+/// <param name="m2"></param>
+/// <returns></returns>
+Matrix4x4 operator-(const Matrix4x4 &m1, const Matrix4x4 &m2);
+
+/// <summary>
+/// 行列の積
+/// </summary>
+/// <param name="m1"></param>
+/// <param name="m2"></param>
+/// <returns></returns>
+Matrix4x4 operator*(const Matrix4x4 m1, const Matrix4x4 &m2);
